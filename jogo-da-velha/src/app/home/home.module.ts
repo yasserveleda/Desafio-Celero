cb612a5097f3daf6168d8696d7faf022676077a9
yasserveleda <veledinha-a@hotmail.com>
2019-02-10 22:11:27 -0200

    adicionando servico mocado com typeahead

--- jogo-da-velha/src/app/home/home.module.ts
+++ jogo-da-velha/src/app/home/home.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { HomeService } from './home/home.service';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    NgbModule
   ],
   providers: [HomeService],
 })
