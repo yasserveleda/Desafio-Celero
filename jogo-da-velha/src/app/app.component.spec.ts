40c3d1769e3d711cc1f7529bb30d83294b06152c
yasserveleda <veledinha-a@hotmail.com>
2019-02-13 21:20:30 -0200

    adicionando assets

--- jogo-da-velha/src/app/app.component.spec.ts
+++ jogo-da-velha/src/app/app.component.spec.ts
@@ -20,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'jogo-da-velha'`, () => {
+  it(`should have as title 'Jogo da Velha - Marvel API'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('jogo-da-velha');
